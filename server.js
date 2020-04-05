74d49ad5a08e63d921d97f6321fdf04c3cd11da0
ahmadkmal <eng.ahmadkmal@gmail.com>
2020-04-05 19:23:12 +0300

    make weather route

--- server.js
+++ server.js
@@ -19,6 +19,21 @@ app.get('/location',(request,response) =>{
     errorHandeler(error,request,response);
   }
 });
+app.get('/weather',(request,response) =>{
+  // response.status(200).send
+  try{
+    const weatherData = require('./data/darksky.json');
+    // console.log(weatherData.data);
+    weatherData.data.forEach(element => {
+      var weather = new Weather(element);
+      Weather.all.push(weather);
+    });
+    // console.log(Weather.all);
+    response.status(200).json(Weather.all);
+  }catch(error){
+    errorHandeler(error,request,response);
+  }
+});
 app.use('*',notFoundHandeler);
 function notFoundHandeler(request,response){
   response.status(404).send('error 404 page not found');
@@ -26,7 +41,7 @@ function notFoundHandeler(request,response){
 
 
 function errorHandeler(error,request,response){
-  response.status(404).send(`error 404 your entered city not found ${request}`);
+  response.status(404).send(`error 404 your entered city not found ${Weather.all}`);
 }
 
 function Location(city,geoData){
@@ -35,4 +50,13 @@ function Location(city,geoData){
   this.latitude =geoData[0].lat;
   this.longitude = geoData[0].lon;
 }
+// {
+//   "forecast": "Partly cloudy until afternoon.",
+//   "time": "Mon Jan 01 2001"
+// },
+function Weather(obj){
+  this.forecast=obj.weather.description;
+  this.time = new Date(obj.valid_date).toDateString();
+}
+Weather.all =[];
 app.listen(PORT ,() => console.log(`the server is up and running on port : ${PORT}`));
